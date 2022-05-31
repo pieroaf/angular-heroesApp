5a53872dea85056dcdbf910a81271fca5c1c4dfb
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-05-31 13:03:26 -0500

    agregando elementos de angular material: button,icon,sidenav,toolbar

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeoresRoutingModule } from './heores-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { RouterModule } from '@angular/router';
-import { HeoresRoutingModule } from './heores-routing.module';
+
+
 
 
 
@@ -20,7 +25,9 @@ import { HeoresRoutingModule } from './heores-routing.module';
   ],
   imports: [
     CommonModule,
-    HeoresRoutingModule
+    HeoresRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 @NgModule({
   exports: [
-
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
