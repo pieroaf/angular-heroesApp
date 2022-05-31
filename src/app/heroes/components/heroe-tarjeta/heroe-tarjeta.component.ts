efec9f62ba750d9dd17b0bda4a4a16e52d996540
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-05-31 18:09:39 -0500

    crear heroeTarjetaComponent

---
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Heroe } from '../../interface/heroes.interface';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styles: [
+  ]
+})
+export class HeroeTarjetaComponent {
+  @Input() heroe!: Heroe;
+
+  constructor() { }
+
+
+
+}
