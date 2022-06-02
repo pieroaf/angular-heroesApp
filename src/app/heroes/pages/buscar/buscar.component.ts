c5e9c4ab829a55d378610aee4274909dacde95e8
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-06-01 19:03:31 -0500

    mejorando autocomplete cuando no encuentra nada

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -12,7 +12,7 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
   termino: string = "";
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado?: Heroe;
 
   constructor( private heroesService: HeroesService ) { }
 
@@ -20,11 +20,15 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getSugerencias( this.termino )
+    this.heroesService.getSugerencias( this.termino.trim() )
     .subscribe( heroes => this.heroes = heroes )
   }
 
   opcionSeleccionada( event: MatAutocompleteActivatedEvent ){
+    if (!event.option?.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
     const heroe: Heroe = event.option?.value;
     this.termino = heroe.superhero;
     this.heroesService.getHeroeById( heroe.id! ).subscribe( heroe=> this.heroeSeleccionado = heroe )
