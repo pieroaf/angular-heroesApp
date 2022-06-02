de3145a9fbf1c96584454a1cb500e5db8597770f
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-06-02 12:40:11 -0500

    editar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interface/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-agregar',
@@ -28,16 +30,26 @@ export class AgregarComponent implements OnInit {
     publisher: Publisher.DCComics,
     alt_img: '',
   }
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroeById(id) )
+    )
+    .subscribe((heroe) => this.heroe=heroe)
   }
 
   guardar(){
     if( this.heroe.superhero.trim().length === 0 ) {
       return;
     }
-    this.heroesService.agregarHeroe(this.heroe).subscribe( resp => console.log(resp))
+
+    if( this.heroe.id ) {
+      this.heroesService.actualizarHeroe(this.heroe).subscribe(heroe=>console.log(heroe))
+    } else {
+      this.heroesService.agregarHeroe(this.heroe).subscribe( heroe => this.router.navigate(['/heroes/edit', heroe.id]))
+    }
   }
 
 }
