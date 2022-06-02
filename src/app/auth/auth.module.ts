ae2c63d8cc815e2000951fb2a123de36bf271565
Fiorela Azahuanche <fazahuanchef@gmail.com>
2022-06-02 17:22:19 -0500

    pantañlla de login basico

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { AuthRoutingModule } from './auth-routing.module';
     RegistroComponent
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    MaterialModule
   ]
 })
 export class AuthModule { }
